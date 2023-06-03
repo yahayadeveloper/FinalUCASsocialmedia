bd3d7c3932231969145dd1c75c41fc1ce0ab167f
YaLia <yahayaessa8@gmail.com>
2023-06-03 14:17:39 +0300

    Initial commit

---
+++ app/src/main/java/com/yahayaessa/finalucassocialmedia/ChatsActivity.java
@@ -0,0 +1,14 @@
+package com.yahayaessa.finalucassocialmedia;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chats);
+    }
+}
